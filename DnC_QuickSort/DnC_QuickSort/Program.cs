5328fa3144822308077e3b42bce74197034c6a4e
KH8 <reczek.krzysztof@gmail.com>
2014-10-29 17:59:35 +0100

    QuickSort init ...

--- DnC_QuickSort/DnC_QuickSort/Program.cs
+++ DnC_QuickSort/DnC_QuickSort/Program.cs
@@ -1,15 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DnC_QuickSort
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            string line;
+            var counter = 0;
+
+            var list = new Dictionary<int, string>();
+
+            // Read the file and display it line by line.
+            var file = new System.IO.StreamReader("QuickSort.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                list.Add(counter, line);
+                counter++;
+            }
+
+            file.Close();
+
+            var array = new int[counter];
+            foreach (var variable in list) array[variable.Key] = Convert.ToInt32(variable.Value);
+
+            var time = DateTime.Now.Millisecond;
+
+            //var numberOfInversions = SortAndCount(array, out array);
+
+            //Console.WriteLine(numberOfInversions + " counted in: " + (DateTime.Now.Millisecond - time) + " ms");
+            Console.ReadKey();
         }
     }
 }
