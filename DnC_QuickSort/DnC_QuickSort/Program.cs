a8f424ba5cba62bf07dc0145bb77817f221b1e91
KH8 <reczek.krzysztof@gmail.com>
2014-10-29 19:08:49 +0100

    QuickSort implemented

--- DnC_QuickSort/DnC_QuickSort/Program.cs
+++ DnC_QuickSort/DnC_QuickSort/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 
 namespace DnC_QuickSort
 {
@@ -27,10 +28,50 @@ namespace DnC_QuickSort
 
             var time = DateTime.Now.Millisecond;
 
+            QuickSort(array, 0, array.Length - 1);
             //var numberOfInversions = SortAndCount(array, out array);
 
             //Console.WriteLine(numberOfInversions + " counted in: " + (DateTime.Now.Millisecond - time) + " ms");
             Console.ReadKey();
+        }
+
+        static void QuickSort(int[] arrayInts, int startPointer, int stopPointer)
+        {
+            if (stopPointer <= startPointer) return;
+
+            var pivot = 0;
+            var splitPoint = Partition(arrayInts, pivot, startPointer, stopPointer);
+
+            QuickSort(arrayInts, startPointer, splitPoint - 1);
+            QuickSort(arrayInts, splitPoint + 1, stopPointer);
+        }
+
+        static int Partition(int[] arrayInts, int pivotIndex, int startPointer, int stopPointer)
+        {
+            if (stopPointer <= startPointer) return 0;
+
+            Swap(arrayInts, startPointer, pivotIndex);
+
+            var i = startPointer + 1;
+
+            for (var j = i; j <= stopPointer; j++)
+            {
+                if (arrayInts[j] < arrayInts[startPointer])
+                {
+                    Swap(arrayInts, j, i);
+                    i++;
+                }
+            }
+
+            Swap(arrayInts, 0, i-1);
+            return i - 1;
+        }
+
+        static void Swap(int[] array, int index1, int index2)
+        {
+            var storageInt = array[index1];
+            array[index1] = array[index2];
+            array[index2] = storageInt;
         }
     }
 }
