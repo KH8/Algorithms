d44c2c26146320f1510234a192a2a0158142ae05
KH8 <reczek.krzysztof@gmail.com>
2014-10-30 09:01:18 +0100

    QuickSort Madian selection refactored

--- DnC_QuickSort/DnC_QuickSort/Program.cs
+++ DnC_QuickSort/DnC_QuickSort/Program.cs
@@ -5,7 +5,7 @@ namespace DnC_QuickSort
 {
     class Program
     {
-        static Random rand = new Random();
+        //static Random rand = new Random();
 
         static void Main()
         {
@@ -39,7 +39,8 @@ namespace DnC_QuickSort
         {
             if (stopPointer <= startPointer) return 0;
 
-            var pivot = rand.Next(startPointer, stopPointer); //var pivot = Median(arrayInts, startPointer, stopPointer);
+            /*var pivot = rand.Next(startPointer, stopPointer); */
+            var pivot = Median(arrayInts, startPointer, stopPointer);
 
             var splitPoint = Partition(arrayInts, pivot, startPointer, stopPointer);
 
@@ -80,28 +81,19 @@ namespace DnC_QuickSort
         static int Median(int[] arrayInts, int startPointer, int stopPointer)
         {
             var array = new int[3];
-
-            array[0] = arrayInts[startPointer];
-
             var middlePointer = (int)Math.Floor((double)(stopPointer + startPointer) / 2);
 
+            array[0] = arrayInts[startPointer];
             array[1] = arrayInts[middlePointer];
             array[2] = arrayInts[stopPointer];
 
-            if (stopPointer - startPointer < 2)
-            {
-                if (array[0] < array[2]) return startPointer;
-                return stopPointer;
-            }
+            if (array[1] < array[0]) Swap(array, 0, 1); 
+            if (array[2] < array[1]) Swap(array, 1, 2);
+            if (array[1] < array[0]) Swap(array, 0, 1);
 
-            if (array[1] < array[0] && array[0] < array[2]) return startPointer;
-            if (array[2] < array[0] && array[0] < array[1]) return startPointer;
-            if (array[0] < array[1] && array[1] < array[2]) return middlePointer;
-            if (array[2] < array[1] && array[1] < array[0]) return middlePointer;
-            if (array[0] < array[2] && array[2] < array[1]) return stopPointer;
-            if (array[1] < array[2] && array[2] < array[0]) return stopPointer;
-
-            return startPointer;
+            if (array[1] == arrayInts[startPointer]) return startPointer;
+            if (array[1] == arrayInts[middlePointer]) return middlePointer;
+            return stopPointer;
         }
     }
 }
