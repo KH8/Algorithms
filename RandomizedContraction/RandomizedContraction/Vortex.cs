11ce54637de28875ea7bf44933cdf0b4ad6b6c96
KH8 <reczek.krzysztof@gmail.com>
2014-11-07 22:03:25 +0100

    RandomizedContraction agorythm initialized

---
+++ RandomizedContraction/RandomizedContraction/Vortex.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomizedContraction
+{
+    class Vortex
+    {
+        public int Id { get; set; }
+        public List<Vortex> EdgesList;
+
+        public int NunberOfedges
+        {
+            get { return EdgesList.Count; }
+        }
+
+        public Vortex(int id)
+        {
+            Id = id;
+            EdgesList = new List<Vortex>();
+        }
+
+        public void Contract(Vortex vortex)
+        {
+            
+        }
+    }
+}
