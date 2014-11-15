32d4420fce00a19a9d799b0d1d11e47a3d69b133
KH8 <reczek.krzysztof@gmail.com>
2014-11-15 17:20:06 +0100

    SCC's graph builder

--- SCCs/SCCs/Program.cs
+++ SCCs/SCCs/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,44 @@ namespace SCCs
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var graph = BuiltGraph();
+
+        }
+
+        private static Collection<Vertex> BuiltGraph()
+        {
+            var graph = new Collection<Vertex>();
+
+            string line;
+
+            var verticesList = new List<string[]>();
+
+            // Read the file and display it line by line.
+            var file = new System.IO.StreamReader("SCC.txt");
+
+            while ((line = file.ReadLine()) != null) verticesList.Add(line.Split(' '));
+
+            var lastVertex = new Vertex(0);
+
+            foreach (var edge in verticesList)
+            {
+                var vertexId = Convert.ToInt32(edge[0]);
+                var edgeId = Convert.ToInt32(edge[1]);
+
+                if (vertexId == lastVertex.Id)
+                {
+                    lastVertex.Edges.Add(edgeId);
+                    continue;
+                }
+
+                lastVertex = new Vertex(vertexId);
+                lastVertex.Edges.Add(edgeId);
+                graph.Add(lastVertex);
+            }
+
+            return graph;
         }
     }
 }
---
+++ SCCs/SCCs/Vertex.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCCs
+{
+    public class Vertex
+    {
+        public int Id;
+        public Boolean IsExplored;
+        public int LeaderId;
+        public Collection<int> Edges;
+        public int FinishingTime;
+
+        public Vertex(int id)
+        {
+            Id = id;
+            IsExplored = false;
+            LeaderId = 0;
+            Edges = new Collection<int>();
+            FinishingTime = 0;
+        }
+    }
+}
