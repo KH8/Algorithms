3e716d86286c22eb7b139668f9f88fc637eaefd5
KH8 <reczek.krzysztof@gmail.com>
2014-11-23 18:47:43 +0100

    Graph builder

--- Dijkstra/Dijkstra/Program.cs
+++ Dijkstra/Dijkstra/Program.cs
@@ -8,8 +8,34 @@ namespace Dijkstra
 {
     class Program
     {
-        static void Main(string[] args)
+        private static Dictionary<int, List<KeyValuePair<int,int>>> _graph;
+
+        static void Main()
         {
+            _graph = BuiltGraph();
+        }
+
+        private static Dictionary<int, List<KeyValuePair<int, int>>> BuiltGraph()
+        {
+            var graph = new Dictionary<int, List<KeyValuePair<int, int>>>();
+            string line;
+            var verticesList = new List<string[]>();
+            // Read the file and display it line by line.
+            var file = new System.IO.StreamReader("dijkstraData.txt");
+            while ((line = file.ReadLine()) != null) verticesList.Add(line.Split('\t'));
+            foreach (var edge in verticesList)
+            {
+                var vertexId = Convert.ToInt32(edge[0]);
+                graph.Add(vertexId, new List<KeyValuePair<int, int>>());
+
+                for (var i = 1; i < edge.Length; i++)
+                {
+                    if(edge[i] == "") continue;
+                    var pair = edge[i].Split(',');
+                    graph[vertexId].Add(new KeyValuePair<int, int>(Convert.ToInt32(pair[0]), Convert.ToInt32(pair[1])));
+                }
+            }
+            return graph;
         }
     }
 }
