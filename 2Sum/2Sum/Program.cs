df65eec9546ad0a640bd49599845e339330bb199
KH8 <reczek.krzysztof@gmail.com>
2014-11-30 21:14:10 +0100

    Compoutation in parallel

--- 2Sum/2Sum/Program.cs
+++ 2Sum/2Sum/Program.cs
@@ -1,31 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace _2Sum
 {
     class Program
     {
+        public static int Counter;
+
         static void Main()
         {
             var hashSet = BuiltHashSet("2sum.txt");
-            var counter = 0;
 
-            for (Int64 i = -10000; i <= 10000; i++)
+            CheckRange(hashSet, 10000, 10000);
+            Parallel.For(-10, 11, i => CheckRange(hashSet, i*1000, i*1000 + 999));
+
+            Console.WriteLine("The result of computation is: " + Counter);
+            Console.ReadKey();
+        }
+
+        public static void CheckRange(HashSet<Int64> hashSet, int start, int stop)
+        {
+            for (Int64 i = start; i <= stop; i++)
             {
                 foreach (var l in hashSet)
                 {
                     var valueToLookUp = i - l;
                     if (l == valueToLookUp) continue;
                     if (!hashSet.Contains(valueToLookUp)) continue;
-                    counter++;
+                    Counter++;
+                    Console.WriteLine(i + " = " + l + " + " + valueToLookUp + " : " + Counter);
                     break;
                 }
-                Console.WriteLine(i + " : " + counter);
             }
-
-            Console.WriteLine("The result of computation is: " + counter);
-            Console.ReadKey();
         }
 
         /// <summary>
